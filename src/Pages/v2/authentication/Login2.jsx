790fca07ed0d894366811871675f45c4dbddd888
NishantRaj29 <29rnishant@gmail.com>
2024-02-10 19:08:51 +0530

    v2_update

--- src/Pages/v2/authentication/Login2.jsx
+++ src/Pages/v2/authentication/Login2.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import './app.scss'
 
 const Login2 = () => {
   return (
-    <div>Login2</div>
-  )
+    <div className="frame">
+            <div className="main">
+                <div className="welcome">
+                    <span> Welcome To</span>
+                    <span>Prudent Pinnacle</span>
+                </div>
+                <div className="registration">
+                    <span>Login</span>
+                    <form action="" className='form'>
+                        <p>User Name</p>
+                        <input type="email" name="" id="email" />
+                        <p>Password</p>
+                        <input type="password" name="" id="password" />
+                    </form>
+                    <p>New User? <Link to="/v2/register">SignUp</Link></p>
+                    {/* <div className='warn'>{errorMsg}</div> */}
+                    <button><span>Login</span></button>
+                </div>
+            </div>
+        </div>
+  );
 }
 
 export default Login2
--- src/Pages/v2/authentication/Register2.jsx
+++ src/Pages/v2/authentication/Register2.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register2 = () => {
   return (
-    <div>Register2</div>
-  )
+    <div className="frame2">
+            <div className="registration">
+                    <span>Registration</span>
+                    <form action="" className='form'>
+                    <input name="name"  type="text" placeholder="Full name"/>
+                    <input name="email"  type="email" placeholder="Email"/>
+                    <input name="username"  type="text" placeholder="username"/>
+                    <input name="password"  type="password" placeholder="password"/>
+                    <input name="ph"  type="text" placeholder="contact number (optional)"/>
+                    <input name="qualification"  type="text" placeholder="qualification (optional) "/>
+                    {/* <MultipleSelectPlaceholder names={departmentNames} defaultLabel='Department' department={department} setDepartment={setDepartment}/> */}
+
+                    </form>
+                    <p>Already a User ? <Link to="/v2/login">Login</Link></p>
+                    {/* <div className='warn'>{errorMsg}</div> */}
+                    <button><span>Create</span></button>
+              </div>
+        </div>
+  );
 }
 
 export default Register2
