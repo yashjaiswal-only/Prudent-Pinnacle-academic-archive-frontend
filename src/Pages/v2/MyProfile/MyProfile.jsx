50f35d794f804a3a3f424762d2c8cd4ccb39bf29
Yash Jaiswal <yashjaiswal2121@gmail.com>
2024-02-11 18:10:18 +0530

    profile update

--- src/Pages/v2/MyProfile/MyProfile.jsx
+++ src/Pages/v2/MyProfile/MyProfile.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
-import styled from 'styled-components'
-const Container=styled.div`
-    position: absolute;
-    top:6rem;
-    left:0;
-    width:100%;
-    height:100vh;
-    background-color: #113B50;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color:#EEEEEE;
-`
+import React,{useEffect} from 'react'
+import './profile.scss'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import EmailIcon from '@mui/icons-material/Email';
+import PhoneIcon from '@mui/icons-material/Phone';
+import Degree from '@mui/icons-material/School';
+import Department from '@mui/icons-material/AccountBalance';
+
 const MyProfile = () => {
+  const user=useSelector(state=>state.user.currentUser)
+  const navigate=useNavigate();
+  useEffect(()=>{
+    if(user===null) navigate('/v2/login');
+  },[])
   return (
-    <Container>Profile page</Container>
+    <div className='frame'>
+      <div className="details">
+            <div className="heading">
+              <h1>{user.name}</h1>
+              <h2>@{user.username}</h2>
+            </div>
+            <div className="labels">
+              <div className="obj"><EmailIcon fontSize="large"/> {user.email}</div>
+              <div className="obj"><PhoneIcon fontSize="large"/>{user.ph} </div>
+              <div className="obj"><Degree fontSize="large"/> {user.qualification}</div>
+              <div className="obj"><Department fontSize="large"/> {user.department}</div>
+            </div>
+      </div>
+      <div className="pic">
+          <img src={user.avatar}/>
+      </div>
+    </div>
   )
 }
 
