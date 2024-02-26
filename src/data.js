f90c02e7fda7532c6cc16efade35ea03d8b621c6
NishantRaj29 <29rnishant@gmail.com>
2024-02-26 17:40:56 +0530

    Field Update

--- src/data.js
+++ src/data.js
@@ -51,6 +51,98 @@ export const category3={
         'Whether you are the main author',	
         'API Score' 
       ]
+    },
+    {
+      name:'B (i) Books published as single author or as editor',
+      fields:[
+        'S.N.',
+        'Title with page no.',
+        'Type of Book & Authorship',	
+        'Publisher & ISSN/ ISBN No.',
+        'Whether Peer Reviewed',
+        'No. of Co-author & Date of Publication',	
+        'Whether Published by National/ International',
+        'API Score' 
+      ]
+    },
+    {
+      name:'B (ii) Articles/ Chapters published in Books (other than referred journals / conference proceedings)',
+      fields:[
+        'S.N.',
+        'Title with page no.',
+        'Book Title, editor & publisher',	
+        'ISSN/ ISBN No.',
+        'Whether Peer Reviewed',
+        'No. of Co-author & Date of Publication',	
+        'Whether Published by National/ International',
+        'API Score' 
+      ]
+    },
+    {
+      name:'C (i & ii). Ongoing Research Projects / Consultancies',
+      fields:[
+        'S.N.',
+        'Title',
+        'Agency',
+        'Period',
+        'Grant/ Amount Mobilized (Rs Lakhs)',
+        'API Score' 
+      ]
+    },
+    {
+      name:'C (iii & iv) Completed Projects / Consultancies',
+      fields:[
+        'S.N.',
+        'Title',
+        'Agency',
+        'Period',
+        'Grant/ Amount Mobilized (Rs Lakhs)',
+        'Whether Policy Documents/Patent as outcome',
+        'API Score' 
+      ]
+    },
+    {
+      name:'(D) Research Guidance',
+      fields:[
+        'Particulars.',
+        'Number Enrolled',
+        'Thesis Submitted',
+        'Degree Awarded',
+        'API Score' 
+      ]
+    },
+    {
+      name:'E(i) Training Courses, Teaching-Learning-Evaluation Technology Programs, Faculty development Programmes (not less than one week duration) (Max.30 Points)',
+      fields:[
+        'S.N.',
+        'Programme',
+        'Duration',
+        'Organised by',
+        'API Score' 
+      ]
+    },
+    {
+      name:'E (ii) Papers presented in Conferences, Seminars, Workshops, Symposia',
+      fields:[
+        'S.N.',
+        'Title of the paper presented',
+        'Title of Conference/Semi nar etc',
+        'Date(s ) of the event',
+        'Organised by',
+        'Whether International/ National/State/Region al/University or College Level',
+        'API Score' 
+      ]
+    },
+    {
+      name:'E(iii) Invited Lectures and Chairmanships at National or International Conference/ Seminars',
+      fields:[
+        'S.N.',
+        'Title of Lecture/ Academic Session',
+        'Title of Conference/ Seminar etc',
+        'Organised by',
+        'Whether International/ National/State',
+        'API Score' 
+      ]
     }
   ]
 }
