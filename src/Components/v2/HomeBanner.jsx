6151eed86d51d7329593ca88e97aac4f42a5b915
Yash Jaiswal <yashjaiswal2121@gmail.com>
2024-03-26 20:53:56 +0530

    change

--- src/Components/v2/HomeBanner.jsx
+++ src/Components/v2/HomeBanner.jsx
@@ -6,16 +6,24 @@ const Container=styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction:column;
     font-size: 3rem;
     font-style: oblique;
     font-family: math;
-    color: thistle;
+    font-weight:800;
+    color:#352c84;
     background: linear-gradient(45deg, rgba(61,88,230,1) 0%, rgba(111,179,228,1) 33%);
+    span{
+      font-size: 2rem;
+      font-weight:900;
+    }
 `
 const HomeBanner = () => {
   return (
     <Container>
       Welcome to Prudent Pinnacle
+      <span>Apex of Scholarly Prudence</span>
+      <span style={{fontSize:'1.5rem'}}>- an academic archive</span>
     </Container>
   )
 }
